7a4ed74572f816c32425bba64c03b224aeae4527
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-26 14:59:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,12 +5,18 @@ class Song
 	extend Concerns::Findable
 
   attr_reader :artist, :genre
+  
+  @@all = []
 
   def initialize(name, artist=nil, genre=nil)
     @name = name
     self.artist = artist if artist != nil
     self.genre = genre if genre != nil
   end
+  
+  def self.all 
+    @@all 
+  end 
 
   def self.create(name)
     instance = Song.new(name)
