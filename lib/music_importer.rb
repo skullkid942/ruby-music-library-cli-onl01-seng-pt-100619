aab0754655ce17e350e37f2df87bda61d9fc3bc1
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-26 14:29:57 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -11,6 +11,12 @@ class MusicImporter
 		files = files.collect{|file|file.scan(/(?<=mp3s\/)(.*)/)}.flatten
   end 
   
-  
+  def import
+		new_files = self.files
+		new_files.each do |file|
+			new_song = Song.create_from_filename(file)
+		end
+
+	end
   
 end
