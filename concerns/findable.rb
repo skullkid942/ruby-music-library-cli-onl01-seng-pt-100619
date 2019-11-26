e99df0c1fd287aabb57bdf8e87fde2fe5a01a9ee
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-26 15:06:47 +0000

    Automatically backed up by Learn

--- concerns/findable.rb
+++ concerns/findable.rb
@@ -16,18 +16,11 @@ module Concerns
 	module Findable
 
 		def find_by_name(name)
-			def find_by_name(name)
     self.all.detect {|song| song.name == name}
-  end
 		end
 
 		def find_or_create_by_name(name)
-			song = find_by_name(name)
-			if song == nil
-				self.create(name)
-			else
-				song
-			end
+			
 		end
 
 	end
